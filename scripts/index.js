cd3db5be592b9015b4a7a5073ffce38ed9acdfc1
Jhonatas Santos <santosjh1@gmail.com>
2022-10-05 23:37:59 +0000

    Add Book and Library classes

---
+++ scripts/index.js
@@ -0,0 +1,73 @@
+class Book {
+  constructor (title, author, pages, isRead) {
+
+    this.title = title || 'Unknown';
+    this.author = author || 'Unknown';
+    this.pages = pages || 'Unknown';
+    this.isRead = isRead || false;
+
+  }
+
+    info() {
+      if (this.read) { 
+        return `${this.title} by ${this.author}, ${this.pages} pages, already read.`;
+      }
+      return `${this.title} by ${this.author}, ${this.pages} pages, not read yet.`;
+    }
+}
+
+class Library {
+  constructor () {
+    this.books = [];
+  }
+
+  #hasBook(wantedBook) {
+    return !!this.books.some(item => item === wantedBook);
+  }
+
+  add(book) {
+    if (!this.#hasBook(book)) {
+      this.books.push(book);
+    }
+  }
+
+  remove(wantedBook) {
+    this.books = this.books.filter(item => item !== wantedBook);
+  }
+}
+
+
+
+// let myLibrary = new Library();
+
+// myLibrary.add('teste');
+
+// myLibrary.add('teste2');
+
+// myLibrary.add('teste');
+
+// console.log(myLibrary);
+
+// myLibrary.remove('teste');
+
+// console.log(myLibrary);
+
+// console.log(myLibrary.#hasBook('teste3'));
+
+
+
+
+
+
+// const book1 = new Book('nome', '', '300', true);
+
+// const book2 = new Book('nome2', 'autor2', '300', false);
+
+// book1.info();
+
+// let json = JSON.stringify(book1, null, 2);
+
+// console.log(book1);
+
+// console.log(book1.info());
+// console.log(book2.info());
