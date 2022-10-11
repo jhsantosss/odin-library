2c7482a8482fdd2fe0c45aef3e3d6e95f43f0560
Jhonatas Santos <santosjh1@gmail.com>
2022-10-11 23:21:04 +0000

    Refactor Library class

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ class Book {
   constructor (title, author, pages, isRead) {
 
     this.title = title;
-    this.author = author || 'Unknown';
-    this.pages = pages || 'Unknown';
+    this.author = author || 'Not provided';
+    this.pages = pages || 'Not provided';
     this.isRead = isRead;
 
   }
@@ -19,99 +19,7 @@ class Book {
 class Library {
   constructor () {
     this.books = [];
-  }
-
-  #hasBook(wantedBook) {
-    return !!this.books.some(item => item.title === wantedBook);
-  }
-
-  #addBook(title, author, pages, isRead) {
-    if (!this.#hasBook(title)) {
-      this.books.push(new Book(title, author, pages, isRead));
-    }
-  }
-
-  #removeBook(wantedBook) {
-    this.books = this.books.filter(item => item.title != wantedBook);
-    this.buildTable();
-  }
-
-  #selectBook(wantedTitle) {
-    const wantedBook = this.books.filter(item => item.title === wantedTitle).at(0);
-    const index = this.books.indexOf(wantedBook);
-    const selectedBook = {
-      book : wantedBook,
-      index : index,
-    }
-    return selectedBook;
-  }
-
-  #editBook(selected) {
-    const bookToBeEdited = selected.book;
-    aux = selected.index;
-    showModal();
-    addBook.value = 'Update';
-
-    const input = this.#getInput();
-
-    input.title.value = bookToBeEdited.title;
-    input.author.value = bookToBeEdited.author;
-    input.pages.value = bookToBeEdited.pages;
-    input.isRead.checked = (bookToBeEdited.isRead === 'Yes') ? true : false;
-
-    
-  }
-
-  #checkEmptTitle() {
-    const inputTitle = document.querySelector('#title');
-
-    if(inputTitle.value.trim().length === 0) {
-      alert('Sorry, title must be provided.');
-      return false;
-    }
-
-    return true;
-  }
-
-  #updateActions() {
-    const deleteButton = document.querySelectorAll('.removeBook');
-    for (let i = 0; i < deleteButton.length; i++) {
-      deleteButton[i].addEventListener('click', () => {
-        this.#removeBook(deleteButton[i].id.replace('-delete', ''));
-      });
-    }
-
-    const editButton = document.querySelectorAll('.editBook');
-    for (let i = 0; i < deleteButton.length; i++) {
-      editButton[i].addEventListener('click', () => {
-        this.#editBook(this.#selectBook(editButton[i].id.replace('-edit', '')));
-      });
-    }
-  }
-
-  #getInput() {
-    const inputTitle = document.querySelector('#title');
-    const inputAuthor = document.querySelector('#author');
-    const inputPages = document.querySelector('#pages');
-    const inputIsRead = document.querySelector('#check');
-    const input = {
-      title : inputTitle,
-      author : inputAuthor,
-      pages : inputPages,
-      isRead : inputIsRead,
-    };
-    return input;
-  }
-
-  #getData() {
-    const input = this.#getInput();
-    const data = {
-      title : input.title.value,
-      author : input.author.value,
-      pages : input.pages.value,
-      isRead : input.isRead.checked ? 'Yes' : 'No',
-    };
-    return data;
+    this.selectedIndex;
   }
 
   getBook() {
@@ -124,18 +32,15 @@ class Library {
 
   updateBook() {
     const data = this.#getData();
-    if (this.#checkEmptTitle()) {
-      this.books[aux].title = data.title;
-      this.books[aux].author = data.author;
-      this.books[aux].pages = data.pages;
-      this.books[aux].isRead = data.isRead;
+    if (this.#checkEmptTitle() && !this.#hasBook(data.title)) {
+      this.books[this.selectedIndex] = new Book(data.title, data.author, data.pages, data.isRead);
       return true;
     }
   }
 
   resetInput() {
-    const forms = document.querySelector('#bookInfo');
-    return forms.reset();
+    const input = this.#getInput();
+    return input.forms.reset();
   }
 
   buildTable() {
@@ -158,6 +63,102 @@ class Library {
     })
     this.#updateActions();
   }
+
+  #addBook(title, author, pages, isRead) {
+    if (!this.#hasBook(title)) {
+      this.books.push(new Book(title, author, pages, isRead));
+    }
+  }
+
+  #checkEmptTitle() {
+    const input = this.#getInput();
+
+    if(input.title.value.trim().length === 0) {
+      alert('Sorry, title must be provided.');
+      return false;
+    }
+
+    return true;
+  }
+
+  #editBookInfo(selectedBook) {
+    const bookToBeEdited = selectedBook;
+
+    toggleModal();
+
+    addBook.value = 'Update';
+
+    const input = this.#getInput();
+
+    input.title.value = bookToBeEdited.title;
+    input.author.value = (bookToBeEdited.author != 'Not provided') ? bookToBeEdited.author : '';
+    input.pages.value = (bookToBeEdited.pages != 'Not provided') ? bookToBeEdited.pages : '';
+    input.isRead.checked = (bookToBeEdited.isRead === 'Yes') ? true : false;
+    
+  }
+
+  #getInput() {
+    const inputForms = document.querySelector('#bookInfo');
+    const inputTitle = document.querySelector('#title');
+    const inputAuthor = document.querySelector('#author');
+    const inputPages = document.querySelector('#pages');
+    const inputIsRead = document.querySelector('#check');
+    const input = {
+      forms: inputForms,
+      title : inputTitle,
+      author : inputAuthor,
+      pages : inputPages,
+      isRead : inputIsRead,
+    };
+    return input;
+  }
+
+  #getData() {
+    const input = this.#getInput();
+    const data = {
+      title : input.title.value,
+      author : input.author.value,
+      pages : input.pages.value,
+      isRead : input.isRead.checked ? 'Yes' : 'No',
+    };
+    return data;
+  }
+
+  #hasBook(wantedBook) {
+    if (!!this.books.some(item => item.title === wantedBook)) {
+      alert ('Sorry, this title has already been registered.')
+      return true;
+    }
+    return false;
+  }
+
+  #removeBook(wantedBook) {
+    this.books = this.books.filter(item => item.title != wantedBook);
+    this.buildTable();
+  }
+
+  #selectBook(wantedTitle) {
+    const selectedBook = this.books.filter(item => item.title === wantedTitle).at(0);
+    this.selectedIndex = this.books.indexOf(selectedBook);
+    
+    return selectedBook;
+  }
+
+  #updateActions() {
+    const deleteButton = document.querySelectorAll('.removeBook');
+    for (let i = 0; i < deleteButton.length; i++) {
+      deleteButton[i].addEventListener('click', () => {
+        this.#removeBook(deleteButton[i].id.replace('-delete', ''));
+      });
+    }
+
+    const editButton = document.querySelectorAll('.editBook');
+    for (let i = 0; i < deleteButton.length; i++) {
+      editButton[i].addEventListener('click', () => {
+        this.#editBookInfo(this.#selectBook(editButton[i].id.replace('-edit', '')));
+      });
+    }
+  }
 }
 
 const myBooks = new Library();
@@ -167,20 +168,21 @@ const modal = document.querySelector("#modal");
 const newBook = document.querySelector("#newBook");
 const closeModal = document.querySelector("#close");
 
-const showModal = () => modal.style.display = "block";
-const hideModal = () => {
-  modal.style.display = "none";
-  resetModal();
-  myBooks.resetInput();
+function toggleModal() {
+  if (modal.style.display === "block") {
+    modal.style.display = "none";
+    resetModal();
+    myBooks.resetInput();
+    return;
+  }
+  modal.style.display = "block";
 }
 
-let aux = '';
+newBook.onclick = toggleModal;
 
-newBook.onclick = showModal;
+closeModal.onclick = toggleModal;
 
-closeModal.onclick = hideModal;
-
-window.onclick = event => { if (event.target == modal) hideModal() };
+window.onclick = event => { if (event.target == modal) toggleModal() };
 
 addBook.onclick = event => {
   event.preventDefault();
@@ -189,13 +191,13 @@ addBook.onclick = event => {
     if (myBooks.updateBook()) {
       myBooks.buildTable();
       myBooks.resetInput();
-      hideModal();
+      toggleModal();
     }
   } else {
       if (myBooks.getBook()) {
         myBooks.buildTable();
         myBooks.resetInput();
-        hideModal();
+        toggleModal();
     }
   }
 }
